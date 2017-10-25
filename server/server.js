97fb70d39ad7e77ae299172f7b4bb99d04a5bf38
alexyu2000 <alexyu2000@gmail.com>
2017-10-25 16:21:32 +0800

    Add Delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -31,7 +31,6 @@ app.get('/todos', (req, res) => {
   });
 });
 
-// GET /todos/123456
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
@@ -43,14 +42,33 @@ app.get('/todos/:id', (req, res) => {
     if (!todo) {
       return res.status(404).send();
     }
+
     res.send({todo});
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-  console.log('Started on port ${port}');
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
